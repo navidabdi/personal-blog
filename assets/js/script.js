0353974142c4c4a0b1d67ae6422b94f2fb882971
navidabdi <artsanaat@gmail.com>
2022-09-09 09:35:19 +0300

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,18 @@ const nav = document.querySelector(".mobile-nav"),
 const navToggleFunc = () => nav.classList.toggle("active");
 navMenuBtn.addEventListener("click", navToggleFunc);
 navCloseBtn.addEventListener("click", navToggleFunc);
+
+// theme toggle variables
+const themeBtn = document.querySelectorAll(".theme-btn");
+
+for (let i = 0; i < themeBtn.length; i++) {
+  themeBtn[i].addEventListener("click", function () {
+    document.body.classList.toggle("light-theme");
+    document.body.classList.toggle("dark-theme");
+
+    for (let j = 0; j < themeBtn.length; j++) {
+      themeBtn[j].classList.toggle("light");
+      themeBtn[j].classList.toggle("dark");
+    }
+  });
+}
